d47aa604c7f429010acd3bf704e1c05bd8e0bfb1
Corentin Ulliac <corentin.ulliac@gmail.com>
2014-10-03 12:12:19 +0200

    First commit, that's dirty.

---
+++ srcs/image.c
@@ -0,0 +1,63 @@
+#include<stdlib.h>
+#include<string.h>
+#include "image.h"
+
+typedef struct{
+unsigned long w;
+unsigned long h;
+char *path;
+unsigned char *buff;
+} image_t;
+
+
+image_t *creer_image(){
+  image_t *img = calloc(1, sizeof(image_t));
+  if(img == NULL){
+    perror("créer_image");
+  }
+  return img;
+}
+
+image_t *copier_image(image_t *src){
+  image_t *dst = creer_image();
+  dst->w = src->w;
+  dst->h = src->h;
+  if(src->path){
+    dst->path = strdup(src->path);
+  }
+  if(dst->path == NULL){
+    perror("copier_image");
+  }
+  if(src->buff){
+    dst->buff = ùammoc(sizeof(char)*scr->w*src->h);
+  }
+  if(dst->buff){
+    memcopy(dst->buff, src->buff, sizeof(char) * src->w * src->h);
+  } else{
+    perror("copier-image")
+  }
+  return dst;
+}
+
+void detruire_image(image_t *p){
+  if(p){
+    if(p->path){
+      free(p->path);
+    }
+    if(p->buff){
+      free(p->buff);
+    }
+    free(p);
+  }
+}
+
+
+// Ici commence le test <-
+int main(){
+  image_t *test = creer_image();
+  image_t *test2 = copier_image();
+  printf("%d %d \n%d %d", test->w, test->h, test2->w, test2->h);
+  detruire_image(test2);
+  detruire_image(test);
+}
+
---
+++ srcs/pgm_image.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#include "image.h"
+#define TMP_STR_SIZE 256
+
+image_t *charger_image_pgm(char *nom_fichier){
+  /*
+  FILE *f;
+  image_t *img;
+  unsigned int nb_ng;
+  char tmp_str[TMP_STR_SIZE];
+  enum format {BIN, ASCII} pgm_form;
+  */
+  
+  
+  return NULL;
+}
+
+int sauver_image_pgm(char *nom_fichier, image_t *img){
+  
+  return 1;
+}
