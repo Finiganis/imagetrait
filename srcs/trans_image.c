da4dd1fd2f36c576dcada0d0834cfd3e8e1211f1
Corentin Ulliac <corentin.ulliac@gmail.com>
2014-10-15 12:40:34 +0200

    Adding functions to trans_image

--- srcs/trans_image.c
+++ srcs/trans_image.c
@@ -73,15 +73,71 @@ image_t *modifier_lumin(image_t *src, int pourcent) {
 
 image_t *bruiter_image(image_t *src, int pourcent) {
   image_t *dst = copier_image2(src);
+  for (int x = 0; x < src->w; x++) {
+    for (int y = 0; y < src->h; y++) {
+      if ((rand() % 100) < pourcent){
+        dst->buff[x + src->w * y] = rand() % 256;
+      } else {
+        dst->buff[x + src->w * y] = src->buff[x + src->w * y];
+      }
+    }
+  }
   return dst;
 }
 
 image_t *filtrer_median(image_t *src) {
   image_t *dst = copier_image2(src);
+  for (int x = 0; x < src->w; x++) {
+    for (int y = 0; y < src->h; y++) {
+      filtrage_median(src, dest, x, y);
+    }
+  }
   return dst;
+}
+
+int somme(noyau *pn) {
+  int res = 0;
+
+  for (int x = 0, x < pn->dim * dim, i++) {
+    res += pn->coeff[x];
+  }
+  return res;
+}
+
+int sortie(int a, int max) {
+  if (a < 0) {
+    return 0;
+  } else if (a > max) {
+    return max;
+  } else {
+    return a;
+  }
+}
+
+void convolution(image_t *src, image_t *dst, noyau *pn, int x, int y) {
+  int convp = 0;
+  const int k = (pn->dim - 1) / 2;
+
+  x -= k;
+  y -= k;
+  for (int i = 0; i < pn->dim, i++) {
+    for (int j = 0; j < pn->dim, j++) {
+      convp += pn->coeffs[i + pn->dim * j] *
+                val_pixel(src, sortie(x + j, src->w) ,
+                          sortie(y + i, src->h));
+    }
+  }
+  convp = convp / (somme(pn));
+  dst->buff[(x + k + src->w * (y + k] = convp;
 }
 
 image_t *convoluer(image_t *src, noyau_t *pn) {
   image_t *dst = copier_image2(src);
+
+  for (int x = 0; x < src->w; x++) {
+    for (int y = 0; y < src->h; y++) {
+      convolution(src, dst, pn, x, y);
+    }
+  }
   return dst;
 }
